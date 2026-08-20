0571b36867d5f830ac066d55d811d07edb990cfd
Armin Ahmadi <armin82004@gmail.com>
2026-08-20 13:30:22 +0330

    fixing auth issues

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -13,32 +14,29 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { toast } from "@/components/ui/toast";
 import { authClient } from "@/lib/auth-client";
-import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Login() {
   const [verified, setVerified] = useState("");
-  const { theme, setTheme } = useTheme();
   const [loading, setLoading] = useState(false);
-  const [mounted, setMounted] = useState(false);
-  const router = useRouter();
   const [invalidUser, setInvalidUser] = useState(false);
+  const [token, setToken] = useState("");
+
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
-  const [token, setToken] = useState("");
+
   useEffect(() => {
     // eslint-disable-next-line react-hooks/set-state-in-effect
-    setMounted(true);
     const params = new URLSearchParams(window.location.search);
-    const verifyParam = new URLSearchParams(window.location.search).get(
-      "verified",
-    );
+
+    const verifyParam = params.get("verified");
+
     setToken(params.get("reset") ?? "");
+
     if (verifyParam) {
       setVerified(verifyParam);
     }
@@ -46,42 +44,55 @@ export default function Login() {
 
   useEffect(() => {
     if (token === "success") {
-      toast.add({ type: "success", description: "پسورد شما ریست شد! 🎉" });
+      toast.add({
+        type: "success",
+        description: "پسورد شما ریست شد! 🎉",
+      });
     }
   }, [token]);
 
   const handleSubmit = async (e: React.SubmitEvent) => {
     e.preventDefault();
+
     setLoading(true);
+    setInvalidUser(false);
+
     const { error } = await authClient.signIn.email({
       email: form.email,
       password: form.password,
+      callbackURL: "/dashboard",
     });
 
     if (error) {
-      if ((error.code = "INVALID_EMAIL_OR_PASSWORD")) {
+      if (error.code === "INVALID_EMAIL_OR_PASSWORD") {
         setInvalidUser(true);
       } else {
         alert(error.message);
       }
+
       setLoading(false);
       return;
     }
-    router.push("/dashboard");
+
+    // Better Auth با callbackURL کاربر را به /dashboard هدایت می‌کند.
   };
+
   return (
     <Card className="w-[calc(100%-2rem)] max-w-sm sm:w-full sm:max-w-md mx-auto dark:bg-neutral-800 rounded-lg sm:rounded-xl h-auto flex flex-col justify-center p-2 sm:p-4">
       <CardHeader className="flex flex-col sm:flex-row justify-center items-center gap-2 sm:gap-3 text-center">
-        <Image src={"/logo.png"} width={35} height={35} alt="Logo" />
+        <Image src="/logo.png" width={35} height={35} alt="Logo" />
+
         <CardTitle className="text-base sm:text-lg lg:text-xl text-balance">
           پرسشنامه ارزیابی آمادگی دیجیتال
         </CardTitle>
       </CardHeader>
+
       {verified && (
         <CardHeader className="justify-center">
           <h1 className="font-bold text-sm sm:text-base text-green-500">
             ایمیل شما تایید شد!
           </h1>
+
           <p className="text-sm sm:text-base leading-relaxed">
             حالا میتونید وارد بشید.
           </p>
@@ -95,37 +106,49 @@ export default function Login() {
           </h1>
         </CardHeader>
       )}
+
       <CardHeader>
         <CardTitle className="text-lg sm:text-xl">ورود</CardTitle>
+
         <CardDescription className="text-xs sm:text-sm">
           ایمیل خود را وارد نمایید.
         </CardDescription>
+
         <CardAction>
-          <Link href={"/signup"}>
-            {" "}
+          <Link href="/signup">
             <Button variant="link" className="dark:text-blue-300">
               ثبت نام
             </Button>
           </Link>
         </CardAction>
       </CardHeader>
+
       <CardContent>
         <form id="login-form" onSubmit={handleSubmit}>
           <div className="flex flex-col gap-4 sm:gap-6">
             <div className="grid gap-2">
               <Label htmlFor="email">ایمیل</Label>
+
               <Input
-                onChange={(e) => setForm({ ...form, email: e.target.value })}
                 id="email"
                 type="email"
                 placeholder="m@example.com"
                 dir="ltr"
                 required
+                value={form.email}
+                onChange={(e) =>
+                  setForm({
+                    ...form,
+                    email: e.target.value,
+                  })
+                }
               />
             </div>
+
             <div className="grid gap-2">
               <div className="flex flex-wrap items-center justify-between gap-1">
                 <Label htmlFor="password">رمز</Label>
+
                 <Link
                   href="/reset-password"
                   className="inline-block text-xs sm:text-sm underline-offset-4 hover:underline"
@@ -133,17 +156,25 @@ export default function Login() {
                   رمز خود را فراموش کرده اید؟
                 </Link>
               </div>
+
               <Input
-                onChange={(e) => setForm({ ...form, password: e.target.value })}
                 id="password"
                 type="password"
                 required
                 dir="ltr"
+                value={form.password}
+                onChange={(e) =>
+                  setForm({
+                    ...form,
+                    password: e.target.value,
+                  })
+                }
               />
             </div>
           </div>
         </form>
       </CardContent>
+
       <CardFooter className="flex flex-col gap-2">
         <Button
           form="login-form"
@@ -153,7 +184,8 @@ export default function Login() {
         >
           {loading ? "در حال ورود..." : "ورود"}
         </Button>
-        <Link className="w-full" href={"/"}>
+
+        <Link className="w-full" href="/">
           <Button variant="outline" className="w-full">
             بازگشت به صفحه اصلی
           </Button>
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -11,44 +12,38 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { authClient } from "@/lib/auth-client";
-import { email } from "better-auth";
-import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 export default function Login() {
-  const { theme, setTheme } = useTheme();
-  const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(false);
+
   const [form, setForm] = useState({
     name: "",
     email: "",
     password: "",
   });
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    // eslint-disable-next-line react-hooks/set-state-in-effect
-    setMounted(true);
-  }, []);
 
   async function handleSubmit(event: React.SubmitEvent) {
     event.preventDefault();
+
     setLoading(true);
+    setSuccess(false);
+
     const { error } = await authClient.signUp.email({
       callbackURL: "/dashboard",
       name: form.name,
       email: form.email,
       password: form.password,
     });
+
     if (error) {
       console.log(error);
       alert(error.message);
+      setLoading(false);
       return;
     }
 
@@ -58,13 +53,14 @@ export default function Login() {
 
   return (
     <Card className="w-[calc(100%-2rem)] max-w-sm sm:w-full sm:max-w-md mx-auto dark:bg-neutral-800 rounded-lg sm:rounded-xl h-auto flex flex-col justify-center p-2 sm:p-4">
-      {" "}
       <CardHeader className="flex flex-col sm:flex-row justify-center items-center gap-2 sm:gap-3 text-center">
-        <Image src={"/logo.png"} width={35} height={35} alt="Logo" />
+        <Image src="/logo.png" width={35} height={35} alt="Logo" />
+
         <CardTitle className="text-base sm:text-lg lg:text-xl text-balance">
           پرسشنامه ارزیابی آمادگی دیجیتال
         </CardTitle>
       </CardHeader>
+
       {success && (
         <CardHeader className="justify-center">
           <h1 className="font-bold text-sm sm:text-base text-green-500">
@@ -72,59 +68,85 @@ export default function Login() {
           </h1>
         </CardHeader>
       )}
+
       <CardHeader>
         <CardTitle className="text-lg sm:text-xl">ثبت نام</CardTitle>
+
         <CardDescription className="text-xs sm:text-sm">
           اطلاعات زیر را تکمیل کنید.
         </CardDescription>
+
         <CardAction>
-          <Link href={"/login"}>
-            {" "}
+          <Link href="/login">
             <Button variant="link" className="dark:text-blue-300">
               ورود
             </Button>
           </Link>
         </CardAction>
       </CardHeader>
+
       <CardContent>
         <form id="register-form" onSubmit={handleSubmit}>
           <div className="flex flex-col gap-4 sm:gap-6">
             <div className="grid gap-2">
               <Label htmlFor="name">نام و نام خانوادگی</Label>
+
               <Input
-                onChange={(e) => setForm({ ...form, name: e.target.value })}
                 id="name"
                 type="text"
                 required
+                value={form.name}
+                onChange={(e) =>
+                  setForm({
+                    ...form,
+                    name: e.target.value,
+                  })
+                }
               />
             </div>
 
             <div className="grid gap-2">
               <Label htmlFor="email">ایمیل</Label>
+
               <Input
-                onChange={(e) => setForm({ ...form, email: e.target.value })}
                 id="email"
                 type="email"
                 placeholder="m@example.com"
                 required
                 dir="ltr"
+                value={form.email}
+                onChange={(e) =>
+                  setForm({
+                    ...form,
+                    email: e.target.value,
+                  })
+                }
               />
             </div>
+
             <div className="grid gap-2">
               <div className="flex items-center justify-between">
                 <Label htmlFor="password">رمز</Label>
               </div>
+
               <Input
-                onChange={(e) => setForm({ ...form, password: e.target.value })}
                 id="password"
                 type="password"
                 required
                 dir="ltr"
+                value={form.password}
+                onChange={(e) =>
+                  setForm({
+                    ...form,
+                    password: e.target.value,
+                  })
+                }
               />
             </div>
           </div>
         </form>
       </CardContent>
+
       <CardFooter className="flex flex-col gap-2">
         <Button
           form="register-form"
@@ -134,7 +156,8 @@ export default function Login() {
         >
           {loading ? "در حال ثبت نام..." : "ثبت نام"}
         </Button>
-        <Link className="w-full" href={"/"}>
+
+        <Link className="w-full" href="/">
           <Button variant="outline" className="w-full">
             بازگشت به صفحه اصلی
           </Button>
